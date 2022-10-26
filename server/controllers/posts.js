4bfafb4c0e7a0010df3c52cbf8a7fbb18cded72b
Z3rodayz <Ec14CAzow6xd>
2022-10-26 21:14:45 +0200

    firstCommit :)

---
+++ server/controllers/posts.js
@@ -0,0 +1,22 @@
+import PostMessage from '../models/postMessage.js'
+
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        console.log(postMessages);
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+
+
+
+    res.send('THIS WORKS!')
+};
+
+export const createPost = (req, res) => {
+    res.send('Post creation')
+}
